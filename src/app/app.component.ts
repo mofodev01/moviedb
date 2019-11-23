f82e49e979030dc6924ecafcfa98c2024c862d82
moustapha fouad <mofodev01@gmail.com>
2019-11-23 23:31:24 +0000

    addshare

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 
 
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -17,6 +18,7 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
+
   ) {
 
     this.initializeApp();
--- src/app/screens/tabs/tabs.page.ts
+++ src/app/screens/tabs/tabs.page.ts
@@ -1,9 +1,10 @@
 /* Core */
 import { Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { Platform,ActionSheetController  } from '@ionic/angular';
 
 /* plugins */
 import { Market } from '@ionic-native/market/ngx';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
 
 
 @Component({
@@ -17,6 +18,9 @@ constructor(
   private market: Market
     
   ,public platform: Platform
+  ,private socialSharing: SocialSharing
+  ,public actionSheetController: ActionSheetController
+
 ) { }
 
 rateApp(){
@@ -30,7 +34,161 @@ rateApp(){
     this.market.open('com.appmofix.movies');
   }
   }
+////share app
 
+async shareapp() {
+  if (this.platform.is('android')) {
+  /*  this.appodeal.hide(this.appodeal.AD_TYPES.BANNER);*/
+    let actionSheet = await  this.actionSheetController.create({
+      header: 'Share App',
+      cssClass: 'action-sheets-basic-page',
+      buttons: [
+        {
+          text: 'Facebook',
+          role: 'destructive',
+          icon: 'logo-facebook',
+         // cssClass: 'action-red',
+          handler: () => {
+           
+              this.socialSharing.shareViaFacebook("", "", "https://play.google.com/store/apps/details?id=com.appmofix.movies").then(() => {
+                console.log("shareViaFacebook: Success");
+              }).catch(() => {
+                console.error("shareViaFacebook: failed");
+              });
+           
+          }
+        },
+     
+      
+        {
+          text: 'Whatsapp',
+          role: 'destructive',
+          icon: 'logo-whatsapp',
+       
+          handler: () => {
+            this.socialSharing.shareViaWhatsApp("", "https://image.prntscr.com/image/G9jvyUxlT7qoNyaFcLfACw.png" ,"https://play.google.com/store/apps/details?id=com.appmofix.movies").then(() => {
+              console.log("shareViaWhatsApp: Success");
+            }).catch(() => {
+              console.error("shareViaWhatsApp: failed");
+            });
+          }
+        },
+        {
+          text: 'Twitter',
+          role: 'destructive',
+          icon: 'logo-twitter',
+       
+          handler: () => {
+            this.socialSharing.shareViaTwitter("", "https://image.prntscr.com/image/G9jvyUxlT7qoNyaFcLfACw.png" ,"https://play.google.com/store/apps/details?id=com.appmofix.movies").then(() => {
+              console.log("shareViatwitter: Success");
+            }).catch(() => {
+              console.error("shareViatwitter: failed");
+            });
+          }
+        },
+        {
+          text: 'Other',
+          role: 'destructive',
+          icon: 'paper-plane',
+        
+          handler: () => {
+            this.socialSharing.share("","", "https://image.prntscr.com/image/G9jvyUxlT7qoNyaFcLfACw.png" ,"https://play.google.com/store/apps/details?id=com.appmofix.movies").then(() => {
+              console.log("shareViatwitter: Success");
+            }).catch(() => {
+              console.error("shareViatwitter: failed");
+            });
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel', // will always sort to be on the bottom
+          icon: 'close' ,
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+}
+ else if (this.platform.is('ios')) {
+    ///  this.appodeal.hide(this.appodeal.AD_TYPES.BANNER);
+      let actionSheet = await this.actionSheetController.create({
+        header: 'Share App',
+        cssClass: 'action-sheets-basic-page',
+        buttons: [
+          {
+            text: 'Facebook',
+            role: 'destructive',
+            icon: 'logo-facebook',
+           // cssClass: 'action-red',
+            handler: () => {
+             
+                this.socialSharing.shareViaFacebook("", "", "https://apps.apple.com/fr/app/apple-store/id375380948").then(() => {
+                  console.log("shareViaFacebook: Success");
+                }).catch(() => {
+                  console.error("shareViaFacebook: failed");
+                });
+             
+            }
+          },
+       
+        
+          {
+            text: 'Whatsapp',
+            role: 'destructive',
+            icon: 'logo-whatsapp',
+         
+            handler: () => {
+              this.socialSharing.shareViaWhatsApp("", "https://image.prntscr.com/image/G9jvyUxlT7qoNyaFcLfACw.png" ,"https://apps.apple.com/fr/app/apple-store/id375380948").then(() => {
+                console.log("shareViaWhatsApp: Success");
+              }).catch(() => {
+                console.error("shareViaWhatsApp: failed");
+              });
+            }
+          },
+          {
+            text: 'Twitter',
+            role: 'destructive',
+            icon: 'logo-twitter',
+         
+            handler: () => {
+              this.socialSharing.shareViaTwitter("", "https://image.prntscr.com/image/G9jvyUxlT7qoNyaFcLfACw.png" ,"https://apps.apple.com/fr/app/apple-store/id375380948").then(() => {
+                console.log("shareViatwitter: Success");
+              }).catch(() => {
+                console.error("shareViatwitter: failed");
+              });
+            }
+          },
+          {
+            text: 'Other',
+            role: 'destructive',
+            icon: 'paper-plane',
+          
+            handler: () => {
+              this.socialSharing.share("","", "https://image.prntscr.com/image/G9jvyUxlT7qoNyaFcLfACw.png" ,"https://apps.apple.com/fr/app/apple-store/id375380948").then(() => {
+                console.log("shareViatwitter: Success");
+              }).catch(() => {
+                console.error("shareViatwitter: failed");
+              });
+            }
+          },
+          {
+            text: 'cancel',
+            role: 'cancel', // will always sort to be on the bottom
+            icon: 'close' ,
+            handler: () => {
+              console.log('Cancel clicked');
+            }
+          }
+        ]
+      });
+      actionSheet.present();
+      }
+
+  }
+
+/////share app
   /*
   rateApp(){
   if (this.platform.is('android')) {
